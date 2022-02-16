257d930181f6ed8f97760a86ceb40fcd247da3ae
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-02-16 16:45:50 -0300

    Create exercício-raiz_potencia.php

---
+++ Capitulo01/exercício-raiz_potencia.php
@@ -0,0 +1,13 @@
+<?php
+
+  $x1 = 2.0;
+  $y1 = 2.0;
+  $x2 = 5.0;
+  $y2 = 5.0;
+  $d = sqrt((pow($x2-$x1, 2)) + (pow($y2-$y1, 2)));
+  $x = pow($x2-$x1, 2) + pow($y2-$y1, 2);
+
+  echo "O resultado é " . number_format($d, 2);
+  echo "<br>O resultado é " . number_format(sqrt($x), 2);//também podemos escrever assim
+
+?>
