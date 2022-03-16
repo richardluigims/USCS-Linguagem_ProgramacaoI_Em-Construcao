698e42b0edc70671c953df0b736124ab26f50f14
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-03-16 12:02:45 -0300

    Create CriarLinha.php

---
+++ Capitulo01/CriarLinha.php
@@ -0,0 +1,13 @@
+<?php
+
+  $a = 5;
+  $b = $a;
+  $c = ($a + $b) * 2;
+
+  printf ('$a = %d', $a);
+  printf ('<br>$b = %d', $b);
+
+  printf ('<br><hr></hr>');
+  printf ('<br><br>$c = %d', $c);
+  
+?>
