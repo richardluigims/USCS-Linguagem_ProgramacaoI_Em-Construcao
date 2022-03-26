377e7f78954c740462a3ba087c04af1e68d68eae
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-03-26 18:56:20 -0300

    Create Break.php

---
+++ Capítulo04/Break.php
@@ -0,0 +1,14 @@
+<?php
+
+  for ($i = 1; $i < 10; $i++)
+  {
+    if ($i == 5)
+    {
+      echo "interrompendo o for <br>";
+      break;
+    }
+    echo "i = " . $i; //imprime os valores de 1 até 4
+    echo "<br>";
+   }
+
+ ?>
